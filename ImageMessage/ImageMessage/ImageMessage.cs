2d8450d855bec5c92fc3308d0d33ca0766261fab
Heitor <heitortoledo@hotmail.com>
2018-07-12 18:37:26 -0300

    Initial commit.
    Added initial project files for first commit.

---
+++ ImageMessage/ImageMessage/ImageMessage.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+
+public static class ImageMessage
+{
+    public static string Decode(string source)
+    {
+        Bitmap bitmap = null;
+        string path = "Message";
+        try
+        {
+            if (Uri.TryCreate(source, UriKind.Absolute, out Uri uri) && uri.Scheme == Uri.UriSchemeHttps)
+            {
+                using (WebClient client = new WebClient())
+                {
+                    client.DownloadFile(uri, path);
+                    bitmap = new Bitmap(path);
+                }
+            }
+            else
+            {
+                bitmap = new Bitmap(source);
+            }
+
+            byte[] message = new byte[bitmap.Width];
+            for (int j = 0; j < bitmap.Width; j++)
+            {
+                message[j] = bitmap.GetPixel(j, 0).R;
+            }
+
+            return Encoding.ASCII.GetString(message);
+        }
+        finally
+        {
+            bitmap?.Dispose();
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+            }
+        }
+    }
+
+    public static void Encode(string contents, string path)
+    {
+        byte[] pixels = Encoding.ASCII.GetBytes(contents);
+
+        int width = pixels.Length;
+        int height = 1;
+        
+        using (Bitmap bitmap = new Bitmap(width, height, PixelFormat.Format8bppIndexed))
+        {
+            ColorPalette palette = bitmap.Palette;
+
+            for (int j = 0; j < 256; j++)
+            {
+                palette.Entries[j] = Color.FromArgb(j, j, j);
+            }
+
+            bitmap.Palette = palette;
+            BitmapData data = bitmap.LockBits(new Rectangle(0, 0, bitmap.Width, bitmap.Height), ImageLockMode.ReadWrite, bitmap.PixelFormat);
+            Marshal.Copy(pixels, 0, data.Scan0, pixels.Length);
+            bitmap.UnlockBits(data);
+
+            if (Directory.Exists(path))
+            {
+                path = Path.Combine(path, "Encoded.png");
+            }
+            else if (!Path.HasExtension(path))
+            {
+                path = path + ".png";
+            }
+
+            bitmap.Save(path, ImageFormat.Png);
+        }
+    }
+}
---
+++ ImageMessage/ImageMessage/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+
+public class Program
+{
+    public static int Main(string[] args)
+    {
+        string operation;
+        string input;
+        string output;
+
+        if (args.Length > 0)
+        {
+            if (args.Length < 3)
+            {
+                Console.WriteLine("Too few arguments.");
+                return -1;
+            }
+
+            switch (args[0])
+            {
+                case "encode":
+                    operation = "encode";
+                    break;
+                case "decode":
+                    operation = "decode";
+                    break;
+                default:
+                    Console.WriteLine($"{args[0]} is not a valid operation.");
+                    return -2;
+            }
+
+            input = args[1];
+            output = args[2];
+        }
+        else
+        {
+            Console.Write("Do you wish to encode (e) or decode (d) a file?: ");
+            char option;
+            do
+            {
+                try
+                {
+                    option = char.ToUpper(Convert.ToChar(Console.ReadLine()));
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Invalid option!");
+                    option = default;
+                }
+            }
+            while (option != 'E' && option != 'D');
+
+            operation = option == 'E' ? "encode" : "decode";
+
+            Console.Write($"Enter the path to the file you wish to {operation}: ");
+            input = Console.ReadLine();
+            Console.Write($"Enter the path to where you want to save the {operation}d file: ");
+            output = Console.ReadLine();
+        }
+
+        if (operation == "encode")
+        {
+            ImageMessage.Encode(File.ReadAllText(input), output);
+        }
+        else
+        {
+            File.WriteAllText(output, ImageMessage.Decode(input));
+        }
+
+        Console.WriteLine("Operation completed!");
+        Console.ReadKey();
+        return 0;
+    }
+}
